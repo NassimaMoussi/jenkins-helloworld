013eece4fa9605e7d315d947016f975de4bd382d
NassimaMoussi <91883866+NassimaMoussi@users.noreply.github.com>
2026-06-08 16:25:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my beauties ! The sun is up wine your body");
+        System.out.println("Hello my beauties ! The sun is up wine your body, the sun is up oooho");
     }
 }
